268ea3241eb5220b515a28b0b264df204480985f
rajharshcr7 <rajharsh.footballer@gmail.com>
2022-02-24 01:28:00 +0530

    separated schema for populate

--- commands/populate.js
+++ commands/populate.js
@@ -24,29 +24,33 @@ module.exports = {
         //     }, { collection: 'all_abbs' }));
         // }
         if (message.guild.id === '765523244332875776' || message.guild.id === '615297658860601403' || message.member.hasPermission('MANAGE_ROLES')) {
-            var data = fs.readFileSync('./commands/abbs.json');
-            var myObject = JSON.parse(data);
-            // const allAbbs = await allabbs();
-            const allAbbs = require('./abbSchema/abbPop');
-            const abbData = await allAbbs.find().populate('populate_data');
+            try {
+                var data = fs.readFileSync('./commands/abbs.json');
+                var myObject = JSON.parse(data);
+                // const allAbbs = await allabbs();
+                const allAbbs = require('./abbSchema/abbPop');
+                const abbData = await allAbbs.find().populate('populate_data');
 
-            const producedAbbs = [];
-            abbData[0].populate_data.forEach(data => {
-                data.values.forEach(div_abb => {
-                    div_abb.push(data.div)
-                    producedAbbs.push(div_abb);
+                const producedAbbs = [];
+                abbData[0].populate_data.forEach(data => {
+                    data.values.forEach(div_abb => {
+                        div_abb.push(data.div)
+                        producedAbbs.push(div_abb);
+                    });
                 });
-            });
 
-            myObject["values"] = producedAbbs
-
-            var newData = JSON.stringify(myObject);
-            fs.writeFile('./commands/abbs.json', newData, (err) => {
-                if (err) {
-                    throw err.message;
-                }
-                message.reply(`Updated!\nTotal data : ${producedAbbs.length}`);
-            });
+                myObject["values"] = producedAbbs;
+                await message.react('✅');
+                var newData = JSON.stringify(myObject);
+                fs.writeFile('./commands/abbs.json', newData, (err) => {
+                    if (err) {
+                        throw err.message;
+                    }
+                    message.reply(`Updated!\nTotal data : ${producedAbbs.length}`);
+                });
+            } catch (err) {
+                message.reply(err.message)
+            }
         } else {
             message.reply(`You can't use this command!`);
         }
