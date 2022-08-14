9ef45bf06b3bdc10ab2e33cdb22a6dc85a449ebf
rajharshcr7 <rajharsh.footballer@gmail.com>
2022-08-14 21:15:05 +0530

    added abbremove command

---
+++ commands/pre-abbremove.js
@@ -0,0 +1,53 @@
+module.exports = {
+    name: 'removeabb',
+    aliases: ['removeabb'],
+    description: 'Removes the clan abb',
+    args: true,
+    length: 1,
+    category: "all",
+    usage: 'clanAbb',
+    missing: ['`clanAbb`'],
+    explanation: 'Ex: wcl removeabb HR\nwhere HR - clanAbb',
+    accessableby: ['League Admins', 'Moderator'],
+    execute: async (message, args) => {
+        if (message.member.hasPermission('MANAGE_SERVER')) {
+            try {
+                const abbCollection = require('./abbSchema/registeredAbbs');
+                const findAbb = await abbCollection.findOne({ abb: args[0].toUpperCase() });
+                if (findAbb) {
+                    const repCollection = require('./repsSchema/repsSchema');
+
+                    var rosterCollection;
+                    if (findAbb.div === 'HEAVY')
+                        rosterCollection = require('./rosterSchemas/rosterSchemaHeavy');
+                    else if (findAbb.div === 'FLIGHT')
+                        rosterCollection = require('./rosterSchemas/rosterSchemaFlight');
+                    else if (findAbb.div === 'ELITE')
+                        rosterCollection = require('./rosterSchemas/rosterSchemaElite');
+                    else if (findAbb.div === 'BLOCKAGE')
+                        rosterCollection = require('./rosterSchemas/rosterSchemaBlockage');
+                    else if (findAbb.div === 'CHAMPIONS')
+                        rosterCollection = require('./rosterSchemas/rosterSchemaChampions');
+                    else if (findAbb.div === 'CLASSIC')
+                        rosterCollection = require('./rosterSchemas/rosterSchemaClassic');
+                    else if (findAbb.div === 'LIGHT')
+                        rosterCollection = require('./rosterSchemas/rosterSchemaLight');
+
+                    // deleting the objects
+                    const clanData = await abbCollection.findOneAndDelete({ abb: args[0].toUpperCase() });
+                    await repCollection.findOneAndDelete({ abb: args[0].toUpperCase() });
+                    await rosterCollection.findOneAndDelete({ abb: args[0].toUpperCase() });
+                    message.reply(`Successfully deleted team:\nDivision - ${clanData.div}\nClan Tag - ${clanData.clanTag}\nClan Name - ${clanData.clanName}`).then((msg) => msg.react('✅'));
+                } else {
+                    message.reply(`No such abb registered till now!`);
+                    return;
+                }
+            } catch (err) {
+                console.log(err);
+                message.reply(err.message);
+            }
+        } else {
+            message.reply(`You can't use this command!`);
+        }
+    }
+}
