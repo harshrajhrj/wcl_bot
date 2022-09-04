090fc31ba07ceb69cced2800f70daf5d5d793a99
SpooKy <spookyhammer2300@gmail.com>
2022-09-04 23:25:21 +0530

    added `getClanByAbb` method.

--- utility/utility.js
+++ utility/utility.js
@@ -13,6 +13,38 @@ class Utility{
     static getKeyByValue(object, value) {
         return Object.keys(object).find(key => object[key] === value);
     };
+
+
+    /**
+     * Get teamName, clanTag, abb, division by clan abb. 
+     * @static
+     * @async
+     * @param {String} abb of the clan.
+     * @returns {Promise<obj>} returns null if not found.
+     */
+     static async getClanByAbb(abb){
+        var obj = {
+            clanTag : "",
+            teamName : "",
+            abb : "",
+            division : ""
+        };
+
+        const fs = require("fs");
+        const abbs = fs.readFileSync("./commands/abbs.json");
+        const j = JSON.parse(abbs).values; // array of array with index 0 = clanTag, 1 = teamName, 2 = abb, 3 = div
+
+        for(let element of j){
+            if (element[2] == abb) {
+                obj.clanTag = element[0];
+                obj.teamName = element[1];
+                obj.abb = element[2];
+                obj.division = element[3];
+                return obj;
+            }
+        }
+        return null;
+    }
 }
 
 module.exports = Utility;
