3d35228b31cbe219779e4ef50cc1d1b1b3119607
SpooKy <spookyhammer2300@gmail.com>
2022-09-08 20:13:42 +0530

    added new commad and sorted command

--- commands/live-help.js
+++ commands/live-help.js
@@ -13,13 +13,14 @@ wcl delsch - delete a scheduled war
 More commands coming soon.........\n\n
 All Warriors of WCL 🙂
 wcl abbs - lists all the clan abbreviations for a division
+wcl attackframe - shows Champion(eSports) attack frame
 wcl reps - lists the clan representative for a clan
 wcl roster - lists the clan's roster
-wcl standings - lists official standing according to a division
-wcl attackframe - shows Champion(eSports) attack frame
 wcl search - searches a player for all available rosters
-wcl vdual - shows all duals in a particular division
+wcl standings - lists official standing according to a division
 wcl stats - shows scheduled war detail with the war stats(if active)
+wcl vdual - shows all duals in a particular division
+wcl viewwars - shows all war scores of a particular clan
 More commands coming soon.........`
         message.channel.send('```plaintext\n' + com + '\n```')
     }
