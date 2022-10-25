47a977e1f057505d5a11bbba34706fbda1541571
rajharshcr7 <rajharsh.footballer@gmail.com>
2022-10-25 20:37:53 +0530

    added the bcl bot channel

--- bcl-commands/live-ForUseChannels.js
+++ bcl-commands/live-ForUseChannels.js
@@ -1,5 +1,5 @@
 class allowedChannel {
-    static rosterChannel = ['1030944845994401852'];
-    static scheduleChannel = ['1030944925157687376'];
+    static rosterChannel = ['1030944845994401852', '1034380073941348433'];
+    static scheduleChannel = ['1030944925157687376', '1034380073941348433'];
 }
 module.exports = allowedChannel;
